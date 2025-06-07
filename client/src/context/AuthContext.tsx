996529466bebc451d1cb8ae8ad127587fab2fac2
Letalandroid <carlossoncra@gmail.com>
2025-06-07 09:37:25 -0500

    Refactor AuthContext para simplificar la gestión de autenticación y eliminar el estado de usuario

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,11 +1,9 @@
 // src/context/AuthContext.tsx
-import { createContext, useState, type ReactNode, useEffect } from "react";
+import { createContext, type ReactNode } from "react";
 import Cookies from "js-cookie";
-import { type User } from "../types";
 
 interface AuthContextType {
-  user: User | null;
-  login: (user: User, token: string) => void;
+  login: (token: string) => void;
   logout: () => void;
 }
 
@@ -14,30 +12,16 @@ export const AuthContext = createContext<AuthContextType>(
 );
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    // Si tienes user en cookie o token, puedes intentar decodificarlo si el backend lo permite
-    const storedUser = Cookies.get("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
-
-  const login = (userData: User, token: string) => {
+  const login = (token: string) => {
     Cookies.set("token", token, { expires: 1 }); // 1 día
-    Cookies.set("user", JSON.stringify(userData), { expires: 1 });
-    setUser(userData);
   };
 
   const logout = () => {
     Cookies.remove("token");
-    Cookies.remove("user");
-    setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ login, logout }}>
       {children}
     </AuthContext.Provider>
   );
