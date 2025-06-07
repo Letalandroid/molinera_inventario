cce3c28bf99e2e5a0a6024b50b56e2c8021bd347
Letalandroid <carlossoncra@gmail.com>
2025-06-07 09:47:24 -0500

    Agregar ruta para editar productos en el enrutador

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import ProductList from "./pages/ProductList";
+import EditProduct from "./pages/EditProduct";
 
 export default function RouterApp() {
   return (
@@ -8,6 +9,7 @@ export default function RouterApp() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/products" element={<ProductList />} />
+        <Route path="/products/edit/:id" element={<EditProduct />} />
       </Routes>
     </BrowserRouter>
   );
