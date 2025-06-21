f9d18d7948f5029c13f420b0a65735ff67936e0d
Letalandroid <carlossoncra@gmail.com>
2025-06-21 18:35:12 -0500

    Agregar módulo y controlador de categoría: implementar CategoryModule, CategoryController y CategoryService, y crear pruebas unitarias para el controlador.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,12 +8,13 @@ import { UsersModule } from './users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ProvidersController } from './providers/providers.controller';
 import { ProvidersModule } from './providers/providers.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), ProductsModule, AuthModule, UsersModule, JwtModule, ProvidersModule],
-  controllers: [AppController, ProvidersController],
+  }), ProductsModule, AuthModule, UsersModule, JwtModule, ProvidersModule, CategoryModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
