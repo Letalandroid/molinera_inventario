53cc7f98932a3cf16811d55cb80bc4812a4d8107
Letalandroid <carlossoncra@gmail.com>
2025-06-23 11:16:07 -0500

    Agregar funcionalidad de cierre de sesión en la barra de navegación y mejorar el estilo del perfil de usuario

--- client/src/components/nav/MainNavbar.tsx
+++ client/src/components/nav/MainNavbar.tsx
@@ -5,9 +5,12 @@ import api from "../../api/api";
 import styles from "../../styles/nav/NavMain.module.css";
 import user_img from "../../assets/user.webp";
 import NavBarLeft from "./NavBarLeft";
+import { useNavigate } from "react-router-dom";
 
 export default function MainNavbar() {
-  const { isAuthenticated, user } = useContext(AuthContext);
+  const { logout, isAuthenticated, user } = useContext(AuthContext);
+  const [closedSession, setClosedSession] = useState(false);
+  const navigate = useNavigate();
 
   const [profile, setProfile] = useState<{ name: string } | null>(null);
 
@@ -28,6 +31,11 @@ export default function MainNavbar() {
     }
   }, [isAuthenticated]);
 
+  const closed_session = () => {
+    logout();
+    navigate("/");
+  };
+
   return (
     <div className={styles.nav_container}>
       <NavBarLeft />
@@ -38,12 +46,30 @@ export default function MainNavbar() {
         </div>
 
         <div className={styles.navSup_user}>
-          {/* Mostrar nombre de usuario si está autenticado */}
           {profile && (
-            <div className={styles.navProfile}>
-              <img src={user_img} />
-              <p>{profile.name}</p>
-              <i className="fas fa-caret-down"></i>
+            <div className={styles.profile_container}>
+              <div
+                onClick={() => setClosedSession(!closedSession)}
+                className={styles.user_data}
+              >
+                <img src={user_img} />
+                <p>{profile.name}</p>
+                <i
+                  style={
+                    !closedSession
+                      ? { transform: "rotateZ(0)" }
+                      : { transform: "rotateZ(180deg)" }
+                  }
+                  className="fas fa-caret-down"
+                ></i>
+              </div>
+              <div
+                onClick={() => closed_session()}
+                style={!closedSession ? { height: 0 } : { height: 35 }}
+                className={styles.closed_session}
+              >
+                <p>Cerrar sesión</p>
+              </div>
             </div>
           )}
         </div>
